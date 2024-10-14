eb227beda6225bb6a6dafdfbcbc05c10389df50c
EJlearner <rzrshrp@gmail.com>
2024-10-14 16:01:55 -0400

    update blurb

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -1,4 +1,4 @@
-import  {createGlobalStyle} from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   :root {
@@ -105,6 +105,10 @@ a {
 
 a:hover {
   text-decoration: underline;
+}
+
+p {
+  margin-bottom: 1em;
 }
 `;
 
