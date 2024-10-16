d5a51118bbe9496d7651e2279a7de5821e741868
EJlearner <rzrshrp@gmail.com>
2024-10-15 21:26:12 -0400

    update donate page

--- react-pages/DonatePage/DonatePage.js
+++ react-pages/DonatePage/DonatePage.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
+import styled from 'styled-components';
 
 import ContentContainer from '../../components/ContentContainer';
 import Layout from '../../components/Layout';
@@ -11,7 +12,80 @@ import supportPicture from '../../images/support-picture.png';
 import {misc} from '../../utils/constants';
 import {useScreenWidthCheck} from '../../utils/useScreenWidthCheck';
 
-import StyledDonatePage from './StyledDonatePage';
+const StyledDonatePage = styled.div`
+  --gray-background: rgb(222, 222, 222);
+  position: relative;
+
+  * {
+    border-radius: 8px;
+  }
+
+  img {
+    border-radius: 0px;
+    height: auto;
+  }
+
+  .top-image {
+    width: 100%;
+    position: absolute;
+  }
+
+  .content-wrapper {
+    position: absolute;
+    width: 100%;
+  }
+
+  .content {
+    margin: 32px 128px;
+    display: flex;
+    gap: 32px;
+    justify-content: center;
+  }
+
+  .image-thanks-wrapper {
+    background-color: var(--gray-background);
+    width: 400px;
+    font-size: 16px;
+
+    .logo-and-text {
+      display: flex;
+      flex-direction: column;
+      padding: 16px 32px;
+    }
+
+    .logo {
+      margin-left: -10px;
+      width: 50px;
+    }
+
+    h2 {
+      margin-top: 8px;
+      font-size: 21px;
+    }
+  }
+
+  a {
+    background-color: var(--accent-yellow);
+    color: var(--black);
+    margin-top: 1em;
+    padding: 4px 8px;
+    text-align: center;
+    display: inline-block;
+    align-self: center;
+  }
+
+  @media screen and (max-width: 800px) {
+    .content {
+      flex-direction: column;
+      margin: 16px;
+      gap: 0;
+    }
+
+    .image-thanks-wrapper {
+      width: 100%;
+    }
+  }
+`;
 
 function DonatePage() {
   const screenIsSmall = useScreenWidthCheck({max: 800});
@@ -32,7 +106,6 @@ function DonatePage() {
           <div className="content-wrapper">
             <div className="content">
               <div className="image-thanks-wrapper">
-                {/* TODO - low priority - figure out why I get Image component warning here */}
                 {!screenIsSmall && (
                   <Image
                     alt="film crew"
@@ -52,10 +125,20 @@ function DonatePage() {
                   )}
                   <h2>Your Support Means The World</h2>
                   <p>
-                    Thank you so very much for your donation—and even more
-                    special—thank you for your support of this project. Your
-                    donation helps us to create quality, impactful films that
-                    emphasize underrepresented perspectives and experiences.
+                    Your donation amplifies and supports the perspectives,
+                    stories, and contributions of Black filmmakers. We create
+                    spaces where these voices are heard, respected, and
+                    celebrated, while also addressing the systemic inequalities
+                    that have historically marginalized them.
+                  </p>
+                  <p>
+                    By uplifting these voices, we can foster greater
+                    understanding, inclusivity, and social change.
+                  </p>
+                  <p>
+                    Thank you so very much for your donation. You’re helping us
+                    to create quality, impactful films that emphasize
+                    underrepresented perspectives and experiences!
                   </p>
 
                   <a href="https://www.gofundme.com/f/help-fund-film-production">
